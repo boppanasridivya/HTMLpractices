5d4b2c5cf779418f3f8cd9400dd81c95f5c132ae
boppanasridivya <boppanasridivya7667@gmail.com>
2025-05-19 23:49:28 +0530

    Adding items to table

--- Revise/RestaurantApp/app.js
+++ Revise/RestaurantApp/app.js
@@ -1,52 +1,110 @@
 const tables=[
-    {id:"table1", name:"table-1",total:0, items:{}},
-    {id:"table2", name:"table-2",total:0, items:{}},
-    {id:"table3", name:"table-3",total:0, items:{}},
+    {id:"table1",name:"table-1",total:0,items:{}},
+    {id:"table2",name:"table-2",total:0,items:{}},
+    {id:"table3",name:"table-3",total:0,items:{}},
+];
 
+const menuItems =[
+    {id:"item1", name:"Biryani",type:"main-course",price:"100"},
+    {id:"item2", name:"Mushroom Manchuria",type:"Starter",price:"200"},
+    {id:"item3", name:"Brownie",type:"Dessert",price:"150"},
 ];
-const menuItems=[
-    {id:"item1", name:"Biryani",price:"150", type:"main-course" },
-    {id:"item2", name:"Pizza",price:"120", type:"main-course" },
-    {id:"item3", name:"Sandwitz",price:"200", type:"main-course" },
-    {id:"item4", name:"Mushroom Manchuria",price:"150", type:"Starter" },
-    {id:"item5", name:"Brownie",price:"250", type:"Dessert" }
-];
-//whenever html page renders I need to get table and menu data on html page
+
 document.addEventListener("DOMContentLoaded", ()=>{
     renderTables(tables);
     renderMenu(menuItems);
-})
-//this is used to get the items in the menu
+});
+//function for menu items
 function renderMenu(menuItems){
-  const menuContainer = document.querySelector("#menu-container");
-  menuContainer.innerHTML = "";
+    const menuContainer = document.getElementById("menu-container");
+    menuContainer.innerHTML="";
 
-  menuItems.forEach((item) =>{
-    const menuItem = document.createElement("div");
-    menuItem.className = "menu-item";
-    menuItem.id = item.id;
-    menuItem.draggable=true;
-    menuItem.dataset.name = item.name;
-    menuItem.dataset.price=item.price;
-    menuItem.dataset.type=item.type;
-//accessing html content u can create document.createElement like that also
+    menuItems.forEach((item) =>{
+        const menuItem= document.createElement("div");
+        menuItem.className ="menu-item";
+        menuItem.id = item.id;
 
-    menuItem.innerHTML=`
-            <h3>${item.name}</h3>
-            <p>${item.price}</p>
-            <p>${item.type}</p>
-    `;
+        menuItem.draggable= true;//allows dragging
+        menuItem.dataset.name = item.name;
+        menuItem.dataset.type = item.type;
+        menuItem.dataset.price = item.price;
 
-    menuContainer.append(menuItem);
-  })
+        menuItem.innerHTML=`
+        <h4>${item.name}</h4>
+        <p>${item.price}</p>
+        <p>Type :${item.type}</p>
+        `;
+        menuContainer.append(menuItem);
+       console.log(menuContainer);
+
+       menuItem.addEventListener("dragstart", dragStart);
+    })
 }
 
-function renderTables((table){
-  const table = document.createElement("div");
-  table.className ="table-num"
-  table.id = table.id;
+//function for tables
+function renderTables(tables){
+    const tableContainer = document.getElementById("table-container");
+    tableContainer.innerHTML="";
 
-  table.innerHTML`
-  
-  `
-})
+    tables.forEach((table) =>{
+        let tableCard = document.createElement("div");
+        tableCard.className="table-card";
+        tableCard.id=table.id;
+
+        tableCard.innerHTML=`
+        <h3>${table.name}</h3>
+        <p>Total:${table.total}</p>
+        <p>Items:${Object.values(table.items) .reduce(
+            (a, b) => a + b.quantity,
+            0
+          )}</p>
+        `
+     tableContainer.append(tableCard);
+     tableCard.addEventListener("dragover", dragOver);
+     tableCard.addEventListener("drop", itemDrop);
+    });
+}
+//it is used to drag the elements from the menu table
+function dragStart(event){
+    event.dataTransfer.setData("text/plain", event.target.id);
+}
+
+//it allows to drag from the emnu and allow where we can drop
+function dragOver(event){
+    event.preventDefault();
+}
+
+//function drop allows to drop the item
+
+function itemDrop(event){
+  event.preventDefault();
+  const itemId = event.dataTransfer.getData("text");
+  const itemElement = document.getElementById(itemId);
+  const itemName = itemElement.dataset.name;
+  const itemPrice = parseFloat(itemElement.dataset.price);
+  const tableId = event.target.id;
+
+  if(itemName && itemPrice && tables.find((table) => table.id === tableId)){
+      addItemToTable(tableId,itemName, itemPrice)
+  }
+}
+function addItemToTable(tableId,itemName,itemPrice){
+    const table = tables.find((table) => table.id === tableId);
+
+    if(!table.items[itemName]){
+        table.items[itemName] = { quantity: 0, price: itemPrice };
+    }
+    table.items[itemName].quantity++;
+    table.total = table.total+itemPrice;
+
+    const tableCard = document.getElementById(tableId);
+    tableCard.querySelector("p").textContent = `Total: ${table.total}`;
+
+    tableCard.querySelector(
+        "p:nth-of-type(2)"
+      ).innerText = `Items: ${Object.values(table.items).reduce(
+        (a, b) => a + b.quantity,
+        0
+      )}`;
+      
+}
