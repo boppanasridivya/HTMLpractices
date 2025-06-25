c2331014e176715a86a073aede4963acd9df4bb7
boppanasridivya <boppanasridivya7667@gmail.com>
2025-06-25 23:58:32 +0530

    updated code

--- Revise/React_practice/Tic-toc-toe/my-tic-toc-toe/src/Component/Board.jsx
+++ Revise/React_practice/Tic-toc-toe/my-tic-toc-toe/src/Component/Board.jsx
@@ -1,28 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 
 const Board = () => {
+
+  const[xIsNext, setXIsNext] = useState(true);
+  const[squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+       if(squares[i]){
+        return;
+       }
+      const nextSquares = squares.slice();
+      if(xIsNext){
+         nextSquares[i] = 'X';
+      }
+      else{
+         nextSquares[i] = 'O';
+      }
+      setSquares(nextSquares);
+      setXIsNext(!xIsNext);
+  }
   return (
     <div>
       <div className="board-section">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[0]} handleClick={()=>handleClick(0)}/>
+        <Square value={squares[1]} handleClick={()=>handleClick(1)}/>
+        <Square value={squares[2]} handleClick={()=>handleClick(2)}/>
       </div>
       <div className="board-section">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[3]} handleClick={()=>handleClick(3)}/>
+        <Square value={squares[4]} handleClick={()=>handleClick(4)}/>
+        <Square value={squares[5]} handleClick={()=>handleClick(5)}/>
 
       </div>
       <div className="board-section">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[6]} handleClick={()=>handleClick(6)}/>
+        <Square value={squares[7]} handleClick={()=>handleClick(7)}/>
+        <Square value={squares[8]} handleClick={()=>handleClick(8)}/>
 
       </div>
     </div>
   )
 }
 
+function calculatorWinner(squares){
+  const items=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+     for(let i=0;i<items.length;i++){
+      const[a,b,c] = lines[i];
+      if(squares[a] && squares[a]===squares[b] && squares[1] === squares[c]){
+          return squares[a];
+      }
+     }
+     return null;
+}
 export default Board
